dd255d91f49ef0edfd6a0c769c5f48c44a5581fa
Annie323 <130882144+Annie323@users.noreply.github.com>
2024-09-26 15:50:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的主页', url: 'https://blog.tangly1024.com' },
-    { title: 'AI爱好者', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的主页', url: '/about' },
+    { title: 'AI爱好者', url: '/about' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -47,11 +47,11 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
+  HEO_INFO_CARD_URL1: '/example-5',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL2: '/shangwu-2',
+  HEO_INFO_CARD_ICON2: 'fa-solid fa-heart',
+  HEO_INFO_CARD_URL3: '/about',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -118,7 +118,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: '/wm',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
